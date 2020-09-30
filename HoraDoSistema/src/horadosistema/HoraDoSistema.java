1411897d428631425052311c741dd05d34727192
mateusfg7 <mateusfelipefg77@gmail.com>
2020-09-30 13:39:52 -0300

    add HoraDoSistema/

---
+++ HoraDoSistema/src/horadosistema/HoraDoSistema.java
@@ -0,0 +1,18 @@
+package horadosistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Mateus
+ */
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é ");
+        System.out.println(relogio.toString());
+    }
+    
+}
